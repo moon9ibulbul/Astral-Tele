87d7b07aa8a2e4baf54a2bcd8e53f98018a25650
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2026-08-14 14:39:43 +0700

    Update config.php

--- config.php
+++ config.php
@@ -19,3 +19,42 @@ $config = [
         'bot_token' => 'YOUR_TELEGRAM_BOT_TOKEN'
     ]
 ];
+
+if (php_sapi_name() !== 'cli') {
+    // Disable default HTML error display to avoid corrupting JSON output
+    ini_set('display_errors', 0);
+    ini_set('html_errors', 0);
+
+    // Register global exception handler to return clean JSON
+    set_exception_handler(function ($exception) {
+        while (ob_get_level() > 0) {
+            ob_end_clean();
+        }
+        header('Content-Type: application/json');
+        http_response_code(500);
+        echo json_encode([
+            'error' => $exception->getMessage(),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine()
+        ]);
+        exit;
+    });
+
+    // Register fatal error shutdown handler to return clean JSON
+    register_shutdown_function(function () {
+        $error = error_get_last();
+        if ($error !== null && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR])) {
+            while (ob_get_level() > 0) {
+                ob_end_clean();
+            }
+            header('Content-Type: application/json');
+            http_response_code(500);
+            echo json_encode([
+                'error' => $error['message'],
+                'file' => $error['file'],
+                'line' => $error['line']
+            ]);
+            exit;
+        }
+    });
+}
